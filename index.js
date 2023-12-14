80c35323438401f72e0f9625cf03f1605632f96a
Richard Rodriguez <rrodriguez@comercializadora-s3.com>
2023-12-14 09:12:41 -0500

    -b

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const sum = (a, b) => a + b;
 
+const schema = yup.object({
+    docType: controller.string().required("El tipo de documento es requerido"),
+    // docNumber: controller.validateDocNumber("docType")
+})
+
 export default {
-    sum
+    sum,
+    schema
 }
