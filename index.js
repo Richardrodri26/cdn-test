dc7c0bbd8dd238b2d778218876a4d127c379a07d
Richard Rodriguez <rrodriguez@comercializadora-s3.com>
2023-12-14 11:25:53 -0500

    - function schema

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import * as yup from 'https://cdn.jsdelivr.net/npm/yup@1.3.2/index.js'
-// import * as yup from 'yup'
+
 const sum = (a, b) => a + b;
 
-const schema = yup.object({
+const schema = (yup) => yup.object({
     docType: yup.string().required("El tipo de documento es requerido"),
     // docNumber: controller.validateDocNumber("docType")
 })
 
-console.log('schema', schema)
 
 const object = { 
     "a": "hola"
